f85e89133d49cc62095e879fedd0ced69286780e
Tony Field <tony@kingsway.digital>
2025-03-27 22:47:11 -0400

    Add schedule item classes

--- src/main/java/ca/team1310/ravenbrain/schedule/ScheduleService.java
+++ src/main/java/ca/team1310/ravenbrain/schedule/ScheduleService.java
@@ -3,8 +3,69 @@
  */
 package ca.team1310.ravenbrain.schedule;
 
+import jakarta.inject.Singleton;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author Tony Field
  * @since 2025-03-23 22:59
  */
-public class ScheduleService {}
+@Slf4j
+@Singleton
+public class ScheduleService {
+  private final List<ScheduleRecord> FAKE_REPO = new ArrayList<>();
+
+  public ScheduleService() {
+    var t = new ScheduleRecord();
+    t.setTournamentId(1);
+    t.setMatch(1);
+    t.setRed1(1310);
+    t.setRed2(9262);
+    t.setRed3(865);
+    t.setBlue1(2056);
+    t.setBlue2(1114);
+    t.setBlue3(1690);
+    FAKE_REPO.add(t);
+
+    t = new ScheduleRecord();
+    t.setTournamentId(1);
+    t.setMatch(2);
+    t.setRed1(1310);
+    t.setRed2(9262);
+    t.setRed3(865);
+    t.setBlue1(2056);
+    t.setBlue2(1114);
+    t.setBlue3(1690);
+    FAKE_REPO.add(t);
+
+    t = new ScheduleRecord();
+    t.setTournamentId(1);
+    t.setMatch(3);
+    t.setRed1(1310);
+    t.setRed2(9262);
+    t.setRed3(865);
+    t.setBlue1(2056);
+    t.setBlue2(1114);
+    t.setBlue3(1690);
+    FAKE_REPO.add(t);
+  }
+
+  void addMatch(ScheduleRecord record) {
+    log.info("Adding match: {}", record);
+    // todo: fixme: implement
+    // table needs to NOT auto-increment the ID field
+
+    FAKE_REPO.add(record);
+  }
+
+  List<ScheduleRecord> listScheduleForTournament(int id) {
+    log.info("Listing schedule for tournament {}", id);
+    // todo: fixme: implement
+    return FAKE_REPO.stream()
+        .filter(record -> record.getTournamentId() == id)
+        .collect(Collectors.toList());
+  }
+}
