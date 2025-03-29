47c8217908297924102e165c1046d7568037c7a4
Tony Field <tony@kingsway.digital>
2025-03-29 13:33:23 -0400

    Convert tournament id to String

--- src/main/java/ca/team1310/ravenbrain/tournament/TournamentRecord.java
+++ src/main/java/ca/team1310/ravenbrain/tournament/TournamentRecord.java
@@ -4,8 +4,8 @@
 package ca.team1310.ravenbrain.tournament;
 
 import io.micronaut.serde.annotation.Serdeable;
-
 import java.time.Instant;
+import java.util.Objects;
 
 /**
  * @author Tony Field
@@ -13,57 +13,60 @@ import java.time.Instant;
  */
 @Serdeable
 public class TournamentRecord {
-    private int id;
-    private String name;
-    private Instant startTime;
-    private Instant endTime;
+  private String id;
+  private String name;
+  private Instant startTime;
+  private Instant endTime;
 
-    public int getId() {
-        return id;
-    }
+  public String getId() {
+    return id;
+  }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+  public void setId(String id) {
+    this.id = id;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public Instant getStartTime() {
-        return startTime;
-    }
+  public Instant getStartTime() {
+    return startTime;
+  }
 
-    public void setStartTime(Instant startTime) {
-        this.startTime = startTime;
-    }
+  public void setStartTime(Instant startTime) {
+    this.startTime = startTime;
+  }
 
-    public Instant getEndTime() {
-        return endTime;
-    }
+  public Instant getEndTime() {
+    return endTime;
+  }
 
-    public void setEndTime(Instant endTime) {
-        this.endTime = endTime;
-    }
+  public void setEndTime(Instant endTime) {
+    this.endTime = endTime;
+  }
 
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
+  @Override
+  public boolean equals(Object o) {
+    if (o == null || getClass() != o.getClass()) return false;
 
-        TournamentRecord that = (TournamentRecord) o;
-        return id == that.id && name.equals(that.name) && startTime.equals(that.startTime) && endTime.equals(that.endTime);
-    }
+    TournamentRecord that = (TournamentRecord) o;
+    return Objects.equals(id, that.id)
+        && Objects.equals(name, that.name)
+        && Objects.equals(startTime, that.startTime)
+        && Objects.equals(endTime, that.endTime);
+  }
 
-    @Override
-    public int hashCode() {
-        int result = id;
-        result = 31 * result + name.hashCode();
-        result = 31 * result + startTime.hashCode();
-        result = 31 * result + endTime.hashCode();
-        return result;
-    }
+  @Override
+  public int hashCode() {
+    int result = Objects.hashCode(id);
+    result = 31 * result + Objects.hashCode(name);
+    result = 31 * result + Objects.hashCode(startTime);
+    result = 31 * result + Objects.hashCode(endTime);
+    return result;
+  }
 }
