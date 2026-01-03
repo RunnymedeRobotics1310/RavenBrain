544946cb3957d339666c0a6133cafb76835b33d8
Tony Field <tony@kingsway.digital>
2026-01-03 15:18:32 -0500

    chore: Change configuration property name and convert config object back to record.

--- src/main/java/ca/team1310/ravenbrain/connect/Config.java
+++ src/main/java/ca/team1310/ravenbrain/connect/Config.java
@@ -4,50 +4,27 @@ import io.micronaut.context.annotation.ConfigurationProperties;
 import io.micronaut.core.annotation.Introspected;
 import io.micronaut.serde.annotation.Serdeable;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 /**
  * @author Tony Field
  * @since 2025-03-29 23:58
  */
-@Data
-@NoArgsConstructor
 @Introspected
 @Serdeable
 @ConfigurationProperties("raven-eye")
-public class Config {
-  private String team;
-  private Security security;
-
-  @Data
-  @NoArgsConstructor
+public record Config(String team, Security security) {
   @Introspected
   @Serdeable
   @ConfigurationProperties("security")
-  public static class Security {
-    private Encryption encryption;
-    private String superuserPassword;
-    private String registrationSecret;
-
-    @Data
-    @NoArgsConstructor
-    @Introspected
-    @Serdeable
-    @ConfigurationProperties("encryption")
-    public static class Encryption {
-      @NotBlank private String seed;
-    }
-  }
+  public record Security(
+      @NotBlank String encryptionSeed, String superuserPassword, String registrationSecret) {}
 
   public String encryptionSeed() {
-    return security != null && security.getEncryption() != null
-        ? security.getEncryption().getSeed()
-        : null;
+    return security != null ? security.encryptionSeed() : null;
   }
 
   public String superuser() {
-    return security != null ? security.getSuperuserPassword() : null;
+    return security != null ? security.superuserPassword() : null;
   }
 
   @Override
