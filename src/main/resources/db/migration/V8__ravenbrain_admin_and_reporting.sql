9faf4e2f21723fcd4be982a4358e0c99959a12ca
Tony Field <tony@kingsway.digital>
2026-01-08 11:08:48 -0500

    feat: Switch comment table to use userid

--- src/main/resources/db/migration/V8__ravenbrain_admin_and_reporting.sql
+++ src/main/resources/db/migration/V8__ravenbrain_admin_and_reporting.sql
@@ -325,115 +325,115 @@ ALTER TABLE RB_EVENT
     ADD COLUMN userid bigint AFTER eventtimestamp;
 
 insert into RB_USER (id, login, display_name, password_hash, roles, enabled, forgot_password)
-VALUES (1, "superuser", "SuperUser", "secret", "ROLE_SUPERUSER", 1, 0);
+VALUES (1, 'superuser', 'SuperUser', 'secret', 'ROLE_SUPERUSER', 1, 0);
 
 insert into RB_USER (id, login, display_name, password_hash, roles, enabled, forgot_password)
-VALUES (2, "Avocado", "Avocado", "secret", "ROLE_ADMIN", 0, 0);
+VALUES (2, 'Avocado', 'Avocado', 'secret', 'ROLE_ADMIN', 0, 0);
 UPDATE RB_EVENT
 set userid = 2
 where scoutname = 'Avocado';
 
 insert into RB_USER (id, login, display_name, password_hash, roles, enabled, forgot_password)
-VALUES (3, "L", "L", "secret", "ROLE_MEMBER", 0, 0);
+VALUES (3, 'L', 'L', 'secret', 'ROLE_MEMBER', 0, 0);
 UPDATE RB_EVENT
 set userid = 3
-where scoutname = "L";
+where scoutname = 'L';
 
 insert into RB_USER (id, login, display_name, password_hash, roles, enabled, forgot_password)
-VALUES (4, "O", "O", "secret", "ROLE_MEMBER", 0, 0);
+VALUES (4, 'O', 'O', 'secret', 'ROLE_MEMBER', 0, 0);
 UPDATE RB_EVENT
 set userid = 4
-where scoutname = "O";
+where scoutname = 'O';
 
 insert into RB_USER (id, login, display_name, password_hash, roles, enabled, forgot_password)
-VALUES (5, "T", "T", "secret", "ROLE_MEMBER", 0, 0);
+VALUES (5, 'T', 'T', 'secret', 'ROLE_MEMBER', 0, 0);
 UPDATE RB_EVENT
 set userid = 5
-where scoutname = "T";
+where scoutname = 'T';
 
 insert into RB_USER (id, login, display_name, password_hash, roles, enabled, forgot_password)
-VALUES (6, "U", "U", "secret", "ROLE_MEMBER", 0, 0);
+VALUES (6, 'U', 'U', 'secret', 'ROLE_MEMBER', 0, 0);
 UPDATE RB_EVENT
 set userid = 6
-where scoutname = "U";
+where scoutname = 'U';
 
 insert into RB_USER (id, login, display_name, password_hash, roles, enabled, forgot_password)
-VALUES (7, "JohnDataScout", "John DataScout", "secret", "ROLE_MEMBER", 0, 0);
+VALUES (7, 'JohnDataScout', 'John DataScout', 'secret', 'ROLE_MEMBER', 0, 0);
 UPDATE RB_EVENT
 set userid = 7
-where scoutname = "John DataScout";
+where scoutname = 'John DataScout';
 
 insert into RB_USER (id, login, display_name, password_hash, roles, enabled, forgot_password)
-VALUES (8, "Quentin", "Quentin", "secret", "ROLE_MEMBER", 0, 0);
+VALUES (8, 'Quentin', 'Quentin', 'secret', 'ROLE_MEMBER', 0, 0);
 UPDATE RB_EVENT
 set userid = 8
-where scoutname = "Quentin";
+where scoutname = 'Quentin';
 
 insert into RB_USER (id, login, display_name, password_hash, roles, enabled, forgot_password)
-VALUES (9, "Tony", "Tony", "secret", "ROLE_MEMBER", 0, 0);
+VALUES (9, 'Tony', 'Tony', 'secret', 'ROLE_MEMBER', 0, 0);
 UPDATE RB_EVENT
 set userid = 9
-where scoutname = "Tony";
+where scoutname = 'Tony';
 
 insert into RB_USER (id, login, display_name, password_hash, roles, enabled, forgot_password)
-VALUES (10, "Y", "Y", "secret", "ROLE_MEMBER", 0, 0);
+VALUES (10, 'Y', 'Y', 'secret', 'ROLE_MEMBER', 0, 0);
 UPDATE RB_EVENT
 set userid = 10
-where scoutname = "Y";
+where scoutname = 'Y';
 
 insert into RB_USER (id, login, display_name, password_hash, roles, enabled, forgot_password)
-VALUES (11, "RachealEng", "Racheal Eng", "secret", "ROLE_MEMBER", 0, 0);
+VALUES (11, 'RachealEng', 'Racheal Eng', 'secret', 'ROLE_MEMBER', 0, 0);
 UPDATE RB_EVENT
 set userid = 11
-where scoutname = "Racheal Eng";
+where scoutname = 'Racheal Eng';
 
 insert into RB_USER (id, login, display_name, password_hash, roles, enabled, forgot_password)
-VALUES (12, "Nick", "Nick", "secret", "ROLE_MEMBER", 0, 0);
+VALUES (12, 'Nick', 'Nick', 'secret', 'ROLE_MEMBER', 0, 0);
 UPDATE RB_EVENT
 set userid = 12
-where scoutname = "Nick";
+where scoutname = 'Nick';
 
 insert into RB_USER (id, login, display_name, password_hash, roles, enabled, forgot_password)
-VALUES (13, "Leia", "Leia", "secret", "ROLE_MEMBER", 0, 0);
+VALUES (13, 'Leia', 'Leia', 'secret', 'ROLE_MEMBER', 0, 0);
 UPDATE RB_EVENT
 set userid = 13
-where scoutname = "Leia";
+where scoutname = 'Leia';
 
 insert into RB_USER (id, login, display_name, password_hash, roles, enabled, forgot_password)
-VALUES (14, "Practice", "Practice", "secret", "ROLE_MEMBER", 0, 0);
+VALUES (14, 'Practice', 'Practice', 'secret', 'ROLE_MEMBER', 0, 0);
 UPDATE RB_EVENT
 set userid = 14
-where scoutname = "Practice";
+where scoutname = 'Practice';
 
 insert into RB_USER (id, login, display_name, password_hash, roles, enabled, forgot_password)
-VALUES (15, "Maya", "Maya", "secret", "ROLE_MEMBER", 0, 0);
+VALUES (15, 'Maya', 'Maya', 'secret', 'ROLE_MEMBER', 0, 0);
 UPDATE RB_EVENT
 set userid = 15
-where scoutname = "Maya";
+where scoutname = 'Maya';
 
 insert into RB_USER (id, login, display_name, password_hash, roles, enabled, forgot_password)
-VALUES (16, "Alan", "Alan", "secret", "ROLE_MEMBER", 0, 0);
+VALUES (16, 'Alan', 'Alan', 'secret', 'ROLE_MEMBER', 0, 0);
 UPDATE RB_EVENT
 set userid = 16
-where scoutname = "Alan";
+where scoutname = 'Alan';
 
 insert into RB_USER (id, login, display_name, password_hash, roles, enabled, forgot_password)
-VALUES (17, "Jackson", "Jackson", "secret", "ROLE_MEMBER", 0, 0);
+VALUES (17, 'Jackson', 'Jackson', 'secret', 'ROLE_MEMBER', 0, 0);
 UPDATE RB_EVENT
 set userid = 17
-where scoutname = "Jackson";
+where scoutname = 'Jackson';
 
 insert into RB_USER (id, login, display_name, password_hash, roles, enabled, forgot_password)
-VALUES (18, "AaronLe", "Aaron Le", "secret", "ROLE_MEMBER", 0, 0);
+VALUES (18, 'AaronLe', 'Aaron Le', 'secret', 'ROLE_MEMBER', 0, 0);
 UPDATE RB_EVENT
 set userid = 18
-where scoutname = "Aaron Le";
+where scoutname = 'Aaron Le';
 
 insert into RB_USER (id, login, display_name, password_hash, roles, enabled, forgot_password)
-VALUES (19, "Meghan", "Meghan", "secret", "ROLE_MEMBER", 0, 0);
+VALUES (19, 'Meghan', 'Meghan', 'secret', 'ROLE_MEMBER', 0, 0);
 UPDATE RB_EVENT
 set userid = 19
-where scoutname = "Meghan";
+where scoutname = 'Meghan';
 
 ALTER TABLE RB_EVENT
     drop constraint time_scout_type;
@@ -444,5 +444,32 @@ ALTER TABLE RB_EVENT
     MODIFY userid BIGINT NOT NULL;
 ALTER TABLE RB_EVENT
     add constraint time_user_type unique (eventtimestamp, userid, eventtype);
+
+
+
+ALTER TABLE RB_COMMENT
+    ADD COLUMN userid bigint AFTER commenttimestamp;
+
+UPDATE RB_COMMENT
+set userid = 9
+where scoutname = 'Tony';
+UPDATE RB_COMMENT
+set userid = 13
+where scoutname = 'Leia';
+UPDATE RB_COMMENT
+set userid = 8
+where scoutname = 'Quentin';
+UPDATE RB_COMMENT
+set userid = 17
+where scoutname = 'Jackson';
+
+ALTER TABLE RB_COMMENT
+    MODIFY userid BIGINT NOT NULL;
+ALTER TABLE RB_COMMENT
+    DROP CONSTRAINT time_scout;
+ALTER TABLE RB_COMMENT
+    add constraint time_user unique (commenttimestamp, userid);
+ALTER TABLE RB_COMMENT
+    DROP COLUMN scoutname;
 
 
