c170da7ce8b37eb5aee18c7cd657c00a787e0881
Tony Field <tony@kingsway.digital>
2026-03-18 21:42:49 -0400

    feat: Sync scouting data from source

---
+++ src/main/java/ca/team1310/ravenbrain/sync/ScoutingDataResult.java
@@ -0,0 +1,13 @@
+package ca.team1310.ravenbrain.sync;
+
+import ca.team1310.ravenbrain.eventlog.EventLogRecord;
+import ca.team1310.ravenbrain.quickcomment.QuickComment;
+import ca.team1310.ravenbrain.robotalert.RobotAlert;
+import io.micronaut.serde.annotation.Serdeable;
+import java.util.List;
+
+@Serdeable
+public record ScoutingDataResult(
+    List<EventLogRecord> events,
+    List<QuickComment> comments,
+    List<RobotAlert> alerts) {}
